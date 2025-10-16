7e11b42263d144f8e20d121c11667cc90c57551b
Oleh Saveniuk <oleh.saveniuk@gmail.com>
2025-10-16 13:54:18 +0300

    Deploy Herdsman game to GitHub Pages

---
+++ node_modules/pixi.js/lib/scene/graphics/shared/utils/buildContextBatches.d.ts
@@ -0,0 +1,3 @@
+import { BatchableGraphics } from '../BatchableGraphics';
+import type { GraphicsContext } from '../GraphicsContext';
+export declare function buildContextBatches(context: GraphicsContext): BatchableGraphics[];
